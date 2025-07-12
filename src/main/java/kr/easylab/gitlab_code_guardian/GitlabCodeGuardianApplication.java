1399a9f8c17d8864b85d0436ade58f413336638d
Seunghwa <banksemi@naver.com>
2025-07-12 12:20:44 +0900

    Spring boot 프로젝트 생성

---
+++ src/main/java/kr/easylab/gitlab_code_guardian/GitlabCodeGuardianApplication.java
@@ -0,0 +1,13 @@
+package kr.easylab.gitlab_code_guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCodeGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCodeGuardianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/easylab/gitlab_code_guardian/GitlabCodeGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.easylab.gitlab_code_guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabCodeGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
